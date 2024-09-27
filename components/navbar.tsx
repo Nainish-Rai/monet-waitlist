32e4954775cdedc3fe923e023f10869c3f9f9ed4
arindammonet <arindam.d@monet.work>
2024-09-27 12:27:26 +0530

    fix: remove window.route

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { MonetWorkLogo } from "./monet-work-logo";
 import { Button } from "./ui/button";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import Link from "next/link";
 
 function Navbar({}) {
@@ -10,6 +10,7 @@ function Navbar({}) {
 
   const isBrand = activePathname === "/";
   const isCustomer = activePathname === "/customer";
+  const router = useRouter();
 
   return (
     <nav className="w-[90%] lg:w-full bg-black/70 rounded-full p-0 lg:bg-transparent my-2 fixed z-50 mx-5 lg:px-0 top-0 ">
@@ -22,14 +23,14 @@ function Navbar({}) {
         <div className="border p-2  bg-black/20 backdrop-blur rounded-full">
           {!isCustomer ? (
             <Button
-              onClick={() => (window.location.href = "/customer")}
+              onClick={() => router.push("/customer")}
               className=" bg-transparent  text-xs lg:text-sm hover:bg-transparent text-white rounded-3xl"
             >
               I&apos;m a customer
             </Button>
           ) : (
             <Button
-              onClick={() => (window.location.href = "/customer")}
+              onClick={() => router.push("/customer")}
               className=" rounded-3xl text-xs lg:text-sm "
             >
               I&apos;m an customer
@@ -37,14 +38,14 @@ function Navbar({}) {
           )}
           {!isBrand ? (
             <Button
-              onClick={() => (window.location.href = "/")}
+              onClick={() => router.push("/")}
               className=" bg-transparent  text-xs lg:text-sm hover:bg-transparent text-white rounded-3xl"
             >
               I&apos;m a brand
             </Button>
           ) : (
             <Button
-              onClick={() => (window.location.href = "/")}
+              onClick={() => router.push("/")}
               className=" rounded-3xl text-xs lg:text-sm "
             >
               I&apos;m an brand
