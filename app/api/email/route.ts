72ea0dd4c9e417250a95269ad3ae137b4b7552e3
anujkmonetwork <anujbrnwal070894@gmail.com>
2024-09-30 12:55:36 +0530

    add email integration

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -5,18 +5,47 @@ export async function POST(req: Request) {
   const { name, contactEmail } = await req.json();
   sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
-  const msg = {
-    to: contactEmail, // Change to your recipient
-    from: "communications@monet.work", // Change to your verified sender
-    subject: "This is a simple message",
-    text: ` Hello, ${name}! This is a simple text message.`,
-    html: "<strong>and some html</strong>",
-  };
+  // const msg = {
+  //   to: contactEmail, // Change to your recipient
+  //   from: "communications@monet.work", // Change to your verified sender
+  //   subject: "This is a simple message",
+  //   text: ` Hello, ${name}! This is a simple text message.`,
+  //   html: "<strong>and some html</strong>",
+  // };
+  // try {
+  //   await sgMail.send(msg);
+  //   console.log("Email sent");
+  // } catch (error) {
+  //   console.error(error);
+  // }
+
   try {
+    const msg = {
+      "from": {
+        "email": "communications@monet.work"
+      },
+      "personalizations": [
+        {
+          "to": [
+            {
+              "email": contactEmail
+            }
+          ],
+          "dynamic_template_data": {
+            "first_name": name
+          }
+        }
+      ],
+      "templateId": "0f9b5c4fec6f41bbada68c24df995acb",
+      "asm": {
+        "groupId": 40581
+      }
+    };
+
     await sgMail.send(msg);
     console.log("Email sent");
-  } catch (error) {
-    console.error(error);
+  } catch (err) {
+    console.log("Failed to send welcome ")
   }
 
   return NextResponse.json({ message: "Email sent" });
---
+++ lib/email.ts
@@ -0,0 +1,48 @@
+import * as sgMail from "@sendgrid/mail";
+
+export async function sendEmail(name: string, email: string) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
+
+    // const msg = {
+    //   to: contactEmail, // Change to your recipient
+    //   from: "communications@monet.work", // Change to your verified sender
+    //   subject: "This is a simple message",
+    //   text: ` Hello, ${name}! This is a simple text message.`,
+    //   html: "<strong>and some html</strong>",
+    // };
+    // try {
+    //   await sgMail.send(msg);
+    //   console.log("Email sent");
+    // } catch (error) {
+    //   console.error(error);
+    // }
+
+    try {
+        const msg = {
+            "from": {
+                "email": "communications@monet.work"
+            },
+            "personalizations": [
+                {
+                    "to": [
+                        {
+                            "email": email
+                        }
+                    ],
+                    "dynamic_template_data": {
+                        "first_name": name
+                    }
+                }
+            ],
+            "templateId": "d-0f9b5c4fec6f41bbada68c24df995acb",
+            "asm": {
+                "groupId": 40581
+            }
+        };
+
+        await sgMail.send(msg);
+        console.log("Email sent");
+    } catch (err) {
+        console.log("Failed to send welcome.");
+    }
+}
