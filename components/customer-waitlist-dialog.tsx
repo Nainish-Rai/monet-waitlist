2150d4b46ad2a27c905f543641243dc99f7cbcb0
arindammonet <arindam.d@monet.work>
2024-10-02 11:32:31 +0530

    fix: revert customer payload

--- components/customer-waitlist-dialog.tsx
+++ components/customer-waitlist-dialog.tsx
@@ -74,7 +74,6 @@ export function CustomerWaitlistDialog({ text }: { text?: string }) {
   });
 
   const [contactPhone, setContactPhone] = useState<string>("");
-  const [country, setCountry] = useState<string>("");
   const { trackSignUp } = useGoogleAnalytics();
 
   const onSubmit = async (data: FormData) => {
@@ -86,7 +85,6 @@ export function CustomerWaitlistDialog({ text }: { text?: string }) {
     } = data;
     let payload: Partial<FormData> = { name, contactEmail, fromWhere };
     if (phoneNumberIntl) {
-      phoneNumberIntl.replace(" ", "");
       const [contactCode] = formatPhoneNumberIntl(phoneNumberIntl).split(" ");
       const contactPhone = formatPhoneNumber(phoneNumberIntl).replace(" ", "");
       payload = { ...payload, contactCode, contactPhone };
@@ -216,14 +214,10 @@ export function CustomerWaitlistDialog({ text }: { text?: string }) {
                 id="contactPhone"
                 value={contactPhone}
                 onChange={(value) => {
-                  console.log(value, "value");
                   setContactPhone(value);
                   setValue("contactPhone", value);
                 }}
                 onCountryChange={(value) => {
-                  console.log(value, "value country");
-                  if(!value) return;
-                  setCountry(value);
                   setValue("contactCode", value);
                 }}
                 defaultCountry="IN"
